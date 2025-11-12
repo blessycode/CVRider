c6c4702ac41f1589adc429b3939940e273014b70
justinmjoh <justin@rowajo.com>
2025-11-11 21:16:24 -0500

    Expanded Jest test suite

--- __tests__/resume.test.ts
+++ __tests__/resume.test.ts
@@ -12,3 +12,55 @@ describe('Test Zod with minimally configured Resume type object...', () => {
         expect(() => resumeSchema.parse(testResume)).not.toThrow(z.ZodError);
     });
 });
+
+describe('Test Zod with incorrectly configured Resume type object...', () => {
+    test("Zod schema parse throws error because education array is empty, but present", () => {
+        const testResume = { // Not valid Resume type (empty education [])
+            name: {
+                first: "Justin"
+            },
+            education: []
+        };
+
+        expect(() => resumeSchema.parse(testResume)).toThrow(z.ZodError);
+    });
+});
+
+describe('Test Zod with incorrectly configured Resume type object...', () => {
+    test("Zod schema parse throws error because experience array is empty, but present", () => {
+        const testResume = { // Not valid Resume type (empty experience [])
+            name: {
+                first: "Justin"
+            },
+            experience: []
+        };
+
+        expect(() => resumeSchema.parse(testResume)).toThrow(z.ZodError);
+    });
+});
+
+describe('Test Zod with incorrectly configured Resume type object...', () => {
+    test("Zod schema parse throws error because projects array is empty, but present", () => {
+        const testResume = { // Not valid Resume type (empty projects [])
+            name: {
+                first: "Justin"
+            },
+            projects: []
+        };
+
+        expect(() => resumeSchema.parse(testResume)).toThrow(z.ZodError);
+    });
+});
+
+describe('Test Zod with incorrectly configured Resume type object...', () => {
+    test("Zod schema parse throws error because skills array is empty, but present", () => {
+        const testResume = { // Not valid Resume type (empty skills [])
+            name: {
+                first: "Justin"
+            },
+            skills: []
+        };
+
+        expect(() => resumeSchema.parse(testResume)).toThrow(z.ZodError);
+    });
+});
