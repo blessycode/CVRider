f0a5a04c5e3b90ae734d12fa144b76bc055747da
Blessed Zhou <zhoublessed16@gmail.com>
2026-01-28 03:18:21 +0200

    config updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.externals = config.externals || [];
-            config.externals.push('pg', 'pg-native');
-        }
-        return config;
-    },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
